f1820ab71b930f3d02763ffa5c3916cc80be7d04
zhangm365 <mzhang2048@gmail.com>
2026-07-29 01:17:23 +0800

    fix

--- fe/fe-core/src/main/java/org/apache/doris/qe/AuditLogHelper.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/AuditLogHelper.java
@@ -66,7 +66,6 @@ import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
